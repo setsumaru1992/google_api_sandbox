355667412d26d39580ea0ee1b52eba302f5c2b85
setsumaru1992 <setsumaru@muriyari.work>
2021-09-23 15:50:11 +0900

    各エンドポイントごとにファイル作成して感情スクリプトコピペ

---
+++ api_scripts/analyze_entities.rb
@@ -0,0 +1,7 @@
+require 'google/cloud/language'
+
+language_service_client = Google::Cloud::Language.language_service version: :v1
+document = { content: "Hello, world!", type: :PLAIN_TEXT }
+response = language_service_client.analyze_entities document: document
+
+pp response
---
+++ api_scripts/analyze_entity_sentiment.rb
@@ -0,0 +1,7 @@
+require 'google/cloud/language'
+
+language_service_client = Google::Cloud::Language.language_service version: :v1
+document = { content: "Hello, world!", type: :PLAIN_TEXT }
+response = language_service_client.analyze_entity_sentiment document: document
+
+pp response
